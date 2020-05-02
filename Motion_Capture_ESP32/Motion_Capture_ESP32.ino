810ff199029fe390b4b0a96de401387b8fc8768e
XnibereiK <m.kierebinski@gmail.com>
2020-05-02 10:02:28 -0700

    Improved sampling speed [100Hz] and consistency of readings

--- Motion_Capture_ESP32/Motion_Capture_ESP32.ino
+++ Motion_Capture_ESP32/Motion_Capture_ESP32.ino
@@ -18,7 +18,7 @@
 #define SENSITIVITY_GYROSCOPE_2000   0.07
 #define PRINT_CALCULATED
 //#define PRINT_RAW
-#define PRINT_SPEED 250
+#define PRINT_SPEED 10
 
 // Keep track of print time
 static unsigned long lastPrint = 0;
@@ -30,10 +30,9 @@ LSM9DS1 imu2;
 //Function prototypes
 void getData();
 void buildPacket();
-void printGyro();
-void printAccel();
-void printMag();
-void printAttitude(float ax, float ay, float az, float mx, float my, float mz);
+//void printGyro();
+//void printAccel();
+//void printAttitude(float ax, float ay, float az, float mx, float my, float mz);
 
 /* WiFi network name and password */
 const char* ssid = "MySpectrumWiFieb-2G";
@@ -44,15 +43,33 @@ const char* password =  "outletwalnut604";
 // here is broadcast address
 const char * udpAddress = "192.168.1.232";
 const int udpPort = 8090;
-struct pkt {float Time, gx1, gy1, gz1, gx2, gy2, gz2, ax1, ay1, az1, ax2, ay2, az2;} pkt;
+struct pkt {float Time, ax1, ay1, az1, gx1, gy1, gz1, ax2, ay2, az2, gx2, gy2, gz2;} pkt;
 //create UDP instance
 WiFiUDP udp;
 char buffer[1024];
+float timestamp = 0;
 
 void setup(){
   Serial.begin(115200);
   Wire.begin(SDA_1, SCL_1, 100000); 
   Wire1.begin(SDA_2, SCL_2, 100000);
+
+
+  if (imu1.begin() == false || imu2.begin(0x6B, 0x1E, Wire1) == false){ // with no arguments, this uses default addresses (AG:0x6B, M:0x1E) and i2c port (Wire).
+     Serial.println("Failed to communicate with LSM9DS1.");
+     Serial.println("Double-check wiring.");
+     Serial.println("Default settings in this sketch will " \
+                   "work for an out of the box LSM9DS1 " \
+                   "Breakout, but may need to be modified " \
+                   "if the board jumpers are.");
+     while (1);
+  }
+       
+  imu1.settings.gyro.sampleRate = 6;
+  imu2.settings.gyro.sampleRate = 6;
+  imu1.settings.accel.sampleRate = 6;
+  imu2.settings.accel.sampleRate = 6;
+  
   //Connect to the WiFi network
   WiFi.begin(ssid, password);
   Serial.println("");
@@ -68,73 +85,49 @@ void setup(){
   Serial.println(WiFi.localIP());
   Serial.println("\nStarting connection to server...");
   //This initializes udp and transfer buffer
-  
   udp.begin(udpPort);
+  
 }
 
-void loop(){
-  if (imu1.begin() == false || imu2.begin(0x6B, 0x1E, Wire1) == false){ // with no arguments, this uses default addresses (AG:0x6B, M:0x1E) and i2c port (Wire).
-     Serial.println("Failed to communicate with LSM9DS1.");
-     Serial.println("Double-check wiring.");
-     Serial.println("Default settings in this sketch will " \
-                   "work for an out of the box LSM9DS1 " \
-                   "Breakout, but may need to be modified " \
-                   "if the board jumpers are.");
-     while (1);
-     }
-  
-  if ((lastPrint) < millis()){
-      //Serial.println("Checking for available data");
-      getData();
-      buildPacket();
-      lastPrint = millis();
-      }
-  //delay(100);
-  //Serial.println("Sending packet...");
-  udp.beginPacket(udpAddress, udpPort);
-  udp.write((byte *) &pkt, sizeof pkt);
-  udp.endPacket();
-  //Serial.println("Packet sent...");
-  //processing incoming packet, must be called before reading the buffer
-  //udp.parsePacket();
-  //receive response from server, it will be HELLO WORLD
-  //if(udp.read(buffer, 50) > 0){
-    //Serial.print("Server to client: ");
-    //Serial.println((char *)buffer);
-  //}
-  //delay(100);
+void loop(){  
+  if ((lastPrint + PRINT_SPEED) <= millis()){
+    //Serial.println("Checking for available data");
+    timestamp = millis();
+    getData();
+    buildPacket();
+    lastPrint = timestamp;
+    
+    //Serial.println("Sending packet...");
+    udp.beginPacket(udpAddress, udpPort);
+    udp.write((byte *) &pkt, sizeof pkt);
+    udp.endPacket();
+    //Serial.println("Packet sent...");
+    //processing incoming packet, must be called before reading the buffer
+  }
 }
 
 //////////////////////FUNCTIONS/////////////////////////////////
 void buildPacket(){
-  pkt.Time = (float)millis();
-  pkt.gx1 = imu1.gx*SENSITIVITY_GYROSCOPE_245;
-  pkt.gy1 = imu1.gy*SENSITIVITY_GYROSCOPE_245;
-  pkt.gz1 = imu1.gz*SENSITIVITY_GYROSCOPE_245;
-  pkt.gx2 = imu2.gx*SENSITIVITY_GYROSCOPE_245;
-  pkt.gy2 = imu2.gy*SENSITIVITY_GYROSCOPE_245;
-  pkt.gz2 = imu2.gz*SENSITIVITY_GYROSCOPE_245;
+  pkt.Time = (float)timestamp;
   pkt.ax1 = imu1.ax*SENSITIVITY_ACCELEROMETER_2;
   pkt.ay1 = imu1.ay*SENSITIVITY_ACCELEROMETER_2;
   pkt.az1 = imu1.az*SENSITIVITY_ACCELEROMETER_2;
+  pkt.gx1 = imu1.gx*SENSITIVITY_GYROSCOPE_245;
+  pkt.gy1 = imu1.gy*SENSITIVITY_GYROSCOPE_245;
+  pkt.gz1 = imu1.gz*SENSITIVITY_GYROSCOPE_245;
   pkt.ax2 = imu2.ax*SENSITIVITY_ACCELEROMETER_2;
   pkt.ay2 = imu2.ay*SENSITIVITY_ACCELEROMETER_2;
   pkt.az2 = imu2.az*SENSITIVITY_ACCELEROMETER_2;
-  }
-
+  pkt.gx2 = imu2.gx*SENSITIVITY_GYROSCOPE_245;
+  pkt.gy2 = imu2.gy*SENSITIVITY_GYROSCOPE_245;
+  pkt.gz2 = imu2.gz*SENSITIVITY_GYROSCOPE_245;
+}
+  
 void getData(){
-          if ( imu1.gyroAvailable() || imu2.gyroAvailable()){
-          // To read from the gyroscope,  first call the
-          // readGyro() function. When it exits, it'll update the
-          // gx, gy, and gz variables with the most current data.
-          imu1.readGyro();
-          imu2.readGyro();
-      }
-          if ( imu1.accelAvailable() || imu2.accelAvailable() ){
-          // To read from the accelerometer, first call the
-          // readAccel() function. When it exits, it'll update the
-          // ax, ay, and az variables with the most current data.
-          imu1.readAccel();
-          imu2.readAccel();
-          }
+  if ( (imu1.gyroAvailable() && imu2.gyroAvailable()) && (imu1.accelAvailable() && imu2.accelAvailable())){
+    imu1.readGyro();
+    imu2.readGyro();
+    imu1.readAccel();
+    imu2.readAccel();
+  }
 }
