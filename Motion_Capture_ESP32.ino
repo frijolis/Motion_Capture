0ba8800f9651889b28cdb0e85927649a97c7641c
XnibereiK <44486763+XnibereiK@users.noreply.github.com>
2020-08-20 19:04:56 -0700

    Added ino file via upload

---
+++ Motion_Capture_ESP32.ino
@@ -0,0 +1,143 @@
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SparkFunLSM9DS1.h>
+
+//Adding definitions for new sensor on I2C
+#define SDA_1 27
+#define SCL_1 26
+#define SDA_2 33
+#define SCL_2 32
+
+//sensitivities can be changed in the functions section
+#define SENSITIVITY_ACCELEROMETER_2  0.000061
+#define SENSITIVITY_ACCELEROMETER_4  0.000122
+#define SENSITIVITY_ACCELEROMETER_8  0.000244
+#define SENSITIVITY_ACCELEROMETER_16 0.000732
+#define SENSITIVITY_GYROSCOPE_245    0.00875
+#define SENSITIVITY_GYROSCOPE_500    0.0175
+#define SENSITIVITY_GYROSCOPE_2000   0.07
+#define PRINT_CALCULATED
+//#define PRINT_RAW
+#define PRINT_SPEED 10 //print speed in ms
+
+// Keep track of print time
+static unsigned long lastPrint = 0;
+
+// Sensor library
+LSM9DS1 imu1;
+LSM9DS1 imu2;
+
+//Function prototypes
+void getData();
+void buildPacket();
+//void printGyro();
+//void printAccel();
+//void printAttitude(float ax, float ay, float az, float mx, float my, float mz);
+
+/* WiFi network name and password */
+const char* ssid = "MySpectrumWiFieb-2G";
+const char* password =  "outletwalnut604";
+//List of IP addresses of computers receiving the data
+const int no_IPs = 3;
+const char * udpAddress[no_IPs] = {"192.168.1.232","192.168.1.67","192.168.1.119"};
+const int udpPort = 8090;
+struct pkt {float Time, ax1, ay1, az1, gx1, gy1, gz1, mx1, my1, mz1, ax2, ay2, az2, gx2, gy2, gz2, mx2, my2, mz2;} pkt;
+//create UDP instance
+WiFiUDP udp;
+char buffer[1024];
+float timestamp = 0;
+
+
+
+void setup(){
+  Serial.begin(115200);
+  Wire.begin(SDA_1, SCL_1, 100000); 
+  Wire1.begin(SDA_2, SCL_2, 100000);
+
+
+  if (imu1.begin() == false || imu2.begin(0x6B, 0x1E, Wire1) == false){ // with no arguments, this uses default addresses (AG:0x6B, M:0x1E) and i2c port (Wire).
+     Serial.println("Failed to communicate with LSM9DS1.");
+     Serial.println("Double-check wiring.");
+     while (1);
+  }
+       
+  imu1.settings.gyro.sampleRate = 6;
+  imu2.settings.gyro.sampleRate = 6;
+  imu1.settings.accel.sampleRate = 6;
+  imu2.settings.accel.sampleRate = 6;
+  imu1.settings.mag.sampleRate = 6;
+  imu2.settings.mag.sampleRate = 6;
+  
+  //Connect to the WiFi network
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("\nStarting connection to server...");
+  //This initializes udp and transfer buffer
+  udp.begin(udpPort);
+}
+
+void loop(){  
+  if ((lastPrint + PRINT_SPEED) <= millis()){
+    //Serial.println("Checking for available data");
+    timestamp = millis();
+    getData();
+    buildPacket();
+    lastPrint = timestamp;
+    for (int i = 0; i < no_IPs; i++)
+    {
+      if (udp.beginPacket(udpAddress[i], udpPort) == 1)
+        {
+        udp.write((byte *) &pkt, sizeof pkt);
+        udp.endPacket();
+        }
+      else Serial.print("UDP Address1 down");
+    }
+  }
+}
+
+//////////////////////FUNCTIONS/////////////////////////////////
+
+void buildPacket(){
+  pkt.Time = (float)timestamp;
+  pkt.ax1 = imu1.calcAccel(imu1.ax);
+  pkt.ay1 = imu1.calcAccel(imu1.ay);
+  pkt.az1 = imu1.calcAccel(imu1.az);
+  pkt.gx1 = imu1.calcGyro(imu1.gx);
+  pkt.gy1 = imu1.calcGyro(imu1.gy);
+  pkt.gz1 = imu1.calcGyro(imu1.gz);
+  pkt.mx1 = imu1.calcMag(imu1.mx);
+  pkt.my1 = imu1.calcMag(imu1.my);
+  pkt.mz1 = imu1.calcMag(imu1.mz);
+  pkt.ax2 = imu2.calcAccel(imu2.ax);
+  pkt.ay2 = imu2.calcAccel(imu2.ay);
+  pkt.az2 = imu2.calcAccel(imu2.az);
+  pkt.gx2 = imu2.calcGyro(imu2.gx);
+  pkt.gy2 = imu2.calcGyro(imu2.gy);
+  pkt.gz2 = imu2.calcGyro(imu2.gz);
+  pkt.mx2 = imu2.calcMag(imu2.mx);
+  pkt.my2 = imu2.calcMag(imu2.my);
+  pkt.mz2 = imu2.calcMag(imu2.mz);
+}
+  
+void getData(){
+  if ( (imu1.gyroAvailable() && imu2.gyroAvailable()) && (imu1.accelAvailable() && imu2.accelAvailable()) && (imu1.magAvailable() && imu2.magAvailable())){
+    imu1.readGyro();
+    imu2.readGyro();
+    imu1.readAccel();
+    imu2.readAccel();
+    imu1.readMag();
+    imu2.readMag();
+  }
+}
